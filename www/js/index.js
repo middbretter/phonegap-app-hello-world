af8a711f59df00ac2e534d54cea3e8e92dce59f8
Brett Crane <crane.brett@gmail.com>
2013-07-13 21:54:51 -0400

    attempt to call geolocation

--- www/js/index.js
+++ www/js/index.js
@@ -33,12 +33,14 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+        app.onSuccess('deviceready');
         navigator.geolocation.getCurrentPosition(onSuccess, onError);
         }
 
         // onSuccess Geolocation
         function onSuccess(position) {
             var element = document.getElementById('geolocation');
+            console.log(element);
             element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
                                 'Longitude: '          + position.coords.longitude             + '<br />' +
                                 'Altitude: '           + position.coords.altitude              + '<br />' +
